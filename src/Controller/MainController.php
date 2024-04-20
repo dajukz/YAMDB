35beaa8ad0712563405cd2fda46f63d4e590eafe
Youke Thomas <youke.thomas@studuent.odisee.be>
2024-04-20 18:42:48 +0200

    basic api request from TMDB most popular list

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,8 +17,23 @@ class MainController extends AbstractController
         $movieName2 = 'dumb and dumber too';
         $movies = [$movieName, $movieName2];
 
+        $client = new Client();
+        try {
+            $response = $client->request('GET', 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', [
+                'headers' => [
+                    'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmNjZDFiMjllYTJlZmE5ZjI4ZGMxNzI4ZTUzMTk4YiIsInN1YiI6IjY2MWZlOGQ0MjE2MjFiMDE2NGYxMDVjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SY-1BPu3v5sMTOd4GQ6BPckYXMzG5P45RR5l8d9XApA',
+                    'accept' => 'application/json',
+                ],
+            ]);
+
+
+            //return new Response($response->getBody().results, 200);
+        } catch (GuzzleException $e) {
+            return new Response('Query failed: ' . $e, 500);
+        }
+
         return $this->render('main/homepage.html.twig', [
-            'movies' => $movies,
+            'movies' => json_decode($response->getBody())->results,
         ]);
     }
 }
