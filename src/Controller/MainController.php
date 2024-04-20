ef7736df27ed36fb92d27ed1c85931ed4b502ead
Youke Thomas <youke.thomas@studuent.odisee.be>
2024-04-20 17:58:23 +0200

    twig test for loop fixed data

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,12 @@ class MainController extends AbstractController
     #[Route('/', name: 'home_page')]
     public function homePage(): Response
     {
-        return $this->render('main/homepage.html.twig');
+        $movieName = 'star wars';
+        $movieName2 = 'dumb and dumber too';
+        $movies = [$movieName, $movieName2];
+
+        return $this->render('main/homepage.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 }
