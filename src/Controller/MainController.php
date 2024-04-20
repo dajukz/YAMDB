acd31a2ff31057952d59f9e725b21189f56b55b9
Youke Thomas <youke.thomas@studuent.odisee.be>
2024-04-20 13:51:38 +0200

    php styling setup php-cs-fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController {
-
+class MainController
+{
     #[Route('/')]
     public function homePage()
     {
         $rand = rand(0, 10);
+
         return new Response(var_dump($rand));
     }
 }
