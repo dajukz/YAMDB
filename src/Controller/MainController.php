96fb39d34cc26763d4fd26538c836963d78a92be
Youke Thomas <youke.thomas@studuent.odisee.be>
2024-04-20 13:46:03 +0200

    basic controller with single function(homePage)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController {
+
+    #[Route('/')]
+    public function homePage()
+    {
+        $rand = rand(0, 10);
+        return new Response(var_dump($rand));
+    }
+}
