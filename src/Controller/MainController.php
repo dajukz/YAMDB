040385717c44215d592470bf6972e18481551fc6
Youke Thomas <youke.thomas@studuent.odisee.be>
2024-04-28 22:50:49 +0200

    reorganisation to  reusable method to fetches movies and twig-render them+search method

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Form\SearchType;
-use App\Repository\MovieRepository;
 use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
@@ -29,10 +28,130 @@ class MainController extends AbstractController
     #[Route('/', name: 'home_page', methods: ['GET'])]
     public function homePage(
         EntityManagerInterface $entityManager,
-        MovieRepository $movieRepository,
         Request $request,
         PaginatorInterface $paginator
     ): Response
+    {
+        return $this->getHomepageView($request, $entityManager, $paginator);
+    }
+
+    /**
+     * Renders the detailed page of a specific movie with a given ID
+     *
+     * @param int $id
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    #[Route('/movie/{id}', name: 'movie_detailed')]
+    public function showById(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $cache = new FilesystemAdapter();
+
+        try {
+            $response = $cache->get($id, function (ItemInterface $item) use ($entityManager, $id) {
+
+                //set expiry to 30min
+                $item->expiresAfter(1800);
+
+                //return movie with the specific id
+                return $entityManager->getRepository(Movie::class)->getMovieById($id);
+            });
+
+            return $this->render('main/detail_page.html.twig', [
+                'movie' => $response,
+            ]);
+
+        } catch (Exception | ORMException | InvalidArgumentException $exception) {
+
+            return new Response('Query failed: '.$exception->getMessage(), 500);
+        }
+    }
+
+
+    /**
+     * runs search and renders results
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param string $term
+     * @return Response
+     */
+    #[Route('/search', name: 'search_movies')]
+    public function search(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get the search term from the request
+            $searchTerm = $request->request->all();
+//            dd($searchTerm['search']);
+            // Redirect to the route that displays the search results
+            return $this->redirectToRoute('search_results', $searchTerm);
+        }
+        try {
+            return $this->getHomepageView($request, $entityManager, $paginator);
+
+        } catch (InvalidArgumentException $e) {
+
+            return new Response('Query failed: '.$e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * return a view of the search results queried from search form
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    #[Route('/search-results', name: 'search_results')]
+    public function searchResults(Request $request, EntityManagerInterface$entityManager, PaginatorInterface $paginator): Response
+    {
+
+        $cache = new FilesystemAdapter();
+        $form = $this->createForm(SearchType::class);
+
+        $searchTerm = $request->query->all()['search']['query'];
+
+        // dd($searchTerm['search']['query']); //access searchterm
+        // Perform search operation using the $searchTerm
+        try {
+            $response = $cache->get($searchTerm, function (ItemInterface $item) use ($entityManager, $searchTerm, $paginator) {
+
+                //set expiry to 30min
+                $item->expiresAfter(1800);
+                $query = $entityManager->getRepository(Movie::class)->getMoviesWithTermQueryBuilder($searchTerm);
+
+                //paginate with query
+                return $paginator->paginate(
+                    $query,
+                    1,
+                    10
+                );
+            });
+        } catch (Exception|ORMException|InvalidArgumentException $exception) {
+            return new Response('Query failed: ' . $exception->getMessage(), 500);
+
+        }
+
+        return $this->render('main/homepage.html.twig', [
+            'pagination' => $response,
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * returns a twig render response depending on the list of movies that need to be rendered
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param PaginatorInterface $paginator
+     * @return Response
+     * @throws InvalidArgumentException
+     */
+    public function getHomepageView(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
     {
         $cache = new FilesystemAdapter();
         $form = $this->createForm(SearchType::class);
@@ -57,10 +176,9 @@ class MainController extends AbstractController
                     $cacheKey,
                     10
                 );
-                //TODO: check of caching mogelijk is per page (van 10)
 
-            } catch (Exception | ORMException | InvalidArgumentException $e) {
-                return new Response('Query failed: '.$e->getMessage(), 500);
+            } catch (Exception|ORMException|InvalidArgumentException $e) {
+                return new Response('Query failed: ' . $e->getMessage(), 500);
             }
 
             return $pagination;
@@ -70,30 +188,5 @@ class MainController extends AbstractController
             'pagination' => $pagination,
             'form' => $form
         ]);
-    }
-
-    #[Route('/movie/{id}', name: 'movie_detailed')]
-    public function showById(int $id, EntityManagerInterface $entityManager)
-    {
-        $cache = new FilesystemAdapter();
-
-        try {
-            $response = $cache->get($id, function (ItemInterface $item) use ($entityManager, $id) {
-
-                //set expiry to 30min
-                $item->expiresAfter(1800);
-
-                //return movie with the specific id
-                return $entityManager->getRepository(Movie::class)->getMovieById($id);
-            });
-
-            return $this->render('main/detail_page.html.twig', [
-                'movie' => $response,
-            ]);
-
-        } catch (Exception | ORMException | InvalidArgumentException $exception) {
-
-            return new Response('Query failed: '.$exception->getMessage(), 500);
-        }
     }
 }
